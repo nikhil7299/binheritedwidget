29931159922ea8a8fb190346e556ec8ba925ad10
nikhil7299 <theavengernikhil@gmail.com>
2022-11-24 01:31:40 +0530

    step-1

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,13 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Main',
+      title: 'Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -26,30 +20,29 @@ class _MyAppState extends State<MyApp> {
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  String title = "Home Page";
+
   @override
   Widget build(BuildContext context) {
-    String title = "Home Page";
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: GestureDetector(
-          onTap: () {
-            setState(() {
-              title = DateTime.now().toIso8601String();
-            });
-          },
-          child: const Center(
-            child: Text('Click Me!'),
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          title = DateTime.now().toIso8601String();
+          // print(title);
+          setState(() {});
+        },
+        child: Container(
+          color: Colors.amber,
         ),
       ),
     );
